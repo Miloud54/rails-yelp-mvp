0bc3b69a9bfe69ef50b29d72b4d8c12afe8c615d
Emilie Didier <emilie.didier97@gmail.com>
2024-11-13 15:11:09 +0100

    02 DONE

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,10 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
+  CATEGORY = %w(chinese italian japanese french belgian)
+
   validates :name, :category, presence: true
   validates :address, :category, presence: true
   validates :category, presence: true
-  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
+  validates :category, inclusion: { in: CATEGORY }
 end
